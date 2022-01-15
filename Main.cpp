d64387e6a6bf8e92cf0d04818b365ce62644a7d8
Tré Dudman <19359682+Tremus@users.noreply.github.com>
2022-01-15 12:08:53 +1100

    3. indices

--- Main.cpp
+++ Main.cpp
@@ -34,6 +34,16 @@ int main()
         -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // lower left corner
         0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,  // lower right corner
         0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // top
+        -0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // inner left
+        0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,  // inner right
+        0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f, // inner bottom
+    };
+
+    GLuint indices[] =
+    {
+        0,3,5, // lower left triangle
+        3,2,4, // lower right triangle
+        5,4,1, // upper triangle
     };
 
     // Create window
@@ -77,15 +87,19 @@ int main()
     glDeleteShader(fragmentShader);
 
     // Create reference containers for the Vertex Array Object and Vertex Buffer Object
-    GLuint VAO, VBO;
+    GLuint VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
     // Makes VAO the current Vertex Array Object by binding it
     glBindVertexArray(VAO);
     // Bind the VBO
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     // Adds the vertices to the VBO
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // Configurations of the the Vertex we're using so OpenGL understands our specific VBO
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -95,6 +109,7 @@ int main()
     // Bind both the VAO and VBO to 0 so we dont accdently modify the VAO/VBO we created
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 
     // Add background colour
@@ -114,7 +129,7 @@ int main()
         // Bind VAO so OpenGL knows about it
         glBindVertexArray(VAO);
         // Draws a triangle using the Triangle promitive
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
         glfwSwapBuffers(window);
 
         // handle GLFW events, such as closing the GLFW window, which breaks the loop
@@ -126,6 +141,7 @@ int main()
     // delete out objects
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
     // Delete the OpenGL window
     glfwDestroyWindow(window);
