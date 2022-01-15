caf014fe786672c3e5bf505837434fc539db8f0d
Tré Dudman <19359682+Tremus@users.noreply.github.com>
2022-01-16 10:07:10 +1100

    7. 3D

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stb/stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "Texture.h"
 #include "shaderClass.h"
@@ -12,17 +15,25 @@
 // Some shape (equalateral triangle)
 GLfloat vertices[] =
 {   // COORDINATES         // COLOURS           // Tex Coord
-    -0.5f, -0.5f, 0.0f,    1.0f, 0.0f, 0.0f,    0.0f, 0.0f, // lower left corner
-    -0.5f,  0.5f, 0.0f,    0.0f, 1.0f, 0.0f,    0.0f, 1.0f, // upper left corner
-     0.5f,  0.5f, 0.0f,    0.0f, 0.0f, 1.0f,    1.0f, 1.0f, // upper right
-     0.5f, -0.5f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 0.0f, // lower right
+    -0.5f, 0.0f,  0.5f,    0.83f, 0.7f,  0.44f,    0.0f, 0.0f,
+    -0.5f, 0.0f, -0.5f,    0.83f, 0.7f,  0.44f,    5.0f, 0.0f,
+     0.5f, 0.0f, -0.5f,    0.83f, 0.7f,  0.44f,    0.0f, 0.0f,
+     0.5f, 0.0f,  0.5f,    0.83f, 0.7f,  0.44f,    5.0f, 0.0f,
+     0.0f, 0.8f,  0.0f,    0.83f, 0.86f, 0.76f,    2.5f, 5.0f,
 };
 
 GLuint indices[] =
 {
-    0,2,1, // upper triangle
-    0,3,2, // lower triangle
+    0,1,2,
+    0,2,3,
+    0,1,4,
+    1,2,4,
+    2,3,4,
+    3,0,4,
 };
+
+const unsigned int width = 800;
+const unsigned int height = 800;
 
 int main()
 {
@@ -36,7 +47,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Create window
-    GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(width, height, "YoutubeOpenGL", NULL, NULL);
     // If creating window failed, exit program
     if (window == NULL)
     {
@@ -51,7 +62,7 @@ int main()
     gladLoadGL();
     
     // Specify the OpenGL viewport (same as window)
-    glViewport(0, 0, 800, 800);
+    glViewport(0, 0, width, height);
 
     // Create shaders
     Shader shaderProgram("default.vert", "default.frag");
@@ -79,32 +90,61 @@ int main()
     GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
     // Texture
-    Texture popCat("pop_cat.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
-    popCat.textUnit(shaderProgram, "tex0", 0);
-    
+    Texture brickTex("brick.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+    brickTex.textUnit(shaderProgram, "tex0", 0);
 
-    GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
-    shaderProgram.Activate();
-    glUniform1i(tex0Uni, 0);
+    // simple timer and accumulator for rotating he pyramid
+    float rotation = 0.0f;
+    double prevTime = glfwGetTime();
 
+    // Enables the Depth Buffer
+    glEnable(GL_DEPTH_TEST);
 
     // Main while loop
     while (!glfwWindowShouldClose(window))
     {
         // Add background colour
         glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-        // Clears the back buffer and assigns the background colour to it
-        glClear(GL_COLOR_BUFFER_BIT);
+        // Clears the back buffer & depth bufferand assigns the background colour to it
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         // Tell OpenGL the Shader Program we're using
         shaderProgram.Activate();
+
+        // check if the pyramid should rotate (60Hz)
+        double crntTime = glfwGetTime();
+        if (crntTime - prevTime >= 1.0 / 60)
+        {
+            rotation += 0.5f;
+            prevTime = crntTime;
+        }
+
+        // Init matrices
+        glm::mat4 model = glm::mat4(1.0f);
+        glm::mat4 view  = glm::mat4(1.0f);
+        glm::mat4 proj  = glm::mat4(1.0f);
+
+        // Assign various tranformations
+        model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0, 0.0f));
+        view = glm::translate(view, glm::vec3(0, -0.5f, -2.0f));
+        // set camera angle, aspect ratio, and field of view
+        proj = glm::perspective(glm::radians(45.0f), (float)(width/ height), 0.1f, 100.0f);
+
+        // Inputs the matrices into the Vertex Shader
+        int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
         // Assigns a value to the uniform (can only be done after the shader program is activated)
         glUniform1f(uniID, 0.5f);
         // Binds texture so it renders
-        popCat.Bind();
+        brickTex.Bind();
         // Bind VAO so OpenGL knows about it
         vao1.Bind();
         // Draws a triangle using the Triangle promitive
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
         // Swaps the front & back buffer, making our BG colour visible
         glfwSwapBuffers(window);
         // handle GLFW events, such as closing the GLFW window, which breaks the loop
@@ -117,7 +157,7 @@ int main()
     vao1.Delete();
     vbo1.Delete();
     ebo1.Delete();
-    popCat.Delete();
+    brickTex.Delete();
     //glDeleteTextures(1, &texture);
     shaderProgram.Delete();
     // Delete the OpenGL window
